5a05ac8e448714c235a1f52e770d4dfd4f5f2c66
Martin Leinweber <102190637+leinweberm@users.noreply.github.com>
2022-06-11 01:50:40 +0200

    creating root files
    creating index.html, style.css, main.js and setting up Vue.js instance

---
+++ main.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+   el: '#app',
+   data: {
+      products: 'Socks',
+      image: './images//vmSocks-green-onWhite.jpg',
+      altText: 'pair of incredibly comfy socks',
+      urlAddress: './index.html'
+   }
+})
