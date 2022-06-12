a3dc4571119546a6518398e554c707d6ee34e141
Martin Leinweber <102190637+leinweberm@users.noreply.github.com>
2022-06-12 10:00:20 +0200

    pridany texty k produktu

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
 var app = new Vue({
    el: '#app',
    data: {
-      products: 'Socks',
+      products: 'Ponožky',
       image: './images//vmSocks-green-onWhite.jpg',
-      altText: 'pair of incredibly comfy socks',
-      urlAddress: './index.html'
-   }
+      altText: 'velmi pohodlný pár ponožek',
+      urlAddress: './index.html',
+      inStock: 9,
+      onSale: true,
+      details: ['velmi pohodlné funkční ponožky', '80% bavlna', 'lze prát vysoké teploty', 'český výrobek'],
+      variants: [
+         {
+            variantId: 2234,
+            variantColor: "green"
+         },
+         {
+            variantId: 2235,
+            variantColor: "blue"
+         }
+      ],
+      sizes: ['35-38', '39-42', '43-46'],
+      cart: 0
+   },
 })
