34decf6e3fc8520794eaf436c0905098327d6932
Martin Leinweber <102190637+leinweberm@users.noreply.github.com>
2022-06-13 19:01:21 +0200

    VueMastery basics 7
    https://www.vuemastery.com/courses/intro-to-vue-js/vue-computed-properties

--- main.js
+++ main.js
@@ -1,24 +1,50 @@
 var app = new Vue({
    el: '#app',
    data: {
+      brand: 'Leinweber',
       products: 'Ponožky',
       image: './images//vmSocks-green-onWhite.jpg',
       altText: 'velmi pohodlný pár ponožek',
       urlAddress: './index.html',
-      inStock: 9,
+      inStock: 11,
       onSale: true,
       details: ['velmi pohodlné funkční ponožky', '80% bavlna', 'lze prát vysoké teploty', 'český výrobek'],
       variants: [
          {
             variantId: 2234,
-            variantColor: "green"
+            variantColor: "green",
+            variantImage: './images/vmSocks-green-onWhite.jpg'
          },
          {
             variantId: 2235,
-            variantColor: "blue"
+            variantColor: "blue",
+            variantImage: './images/vmSocks-blue-onWhite.jpg'
          }
       ],
       sizes: ['35-38', '39-42', '43-46'],
       cart: 0
    },
+   methods: {
+      addToCart: function(){
+         if(this.inStock >= 1){
+            this.inStock--;
+            this.cart++;
+         }  
+      },
+      removeFromCart: function(){
+         
+         if(this.cart >= 1){
+            this.cart--;
+            this.inStock++;
+         }
+      },
+      updateProduct: function(variantImage){
+         this.image = variantImage;
+      }
+   },
+   computed: {
+      title(){
+         return this.brand + ' ' + this.products;
+      }
+   }
 })
